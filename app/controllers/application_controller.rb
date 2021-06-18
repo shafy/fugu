f48c97a888621a4b20875dcd08e4dbfbdca2ee14
Can Olcer <shafyy@gmail.com>
2021-06-18 14:32:49 +0200

    added www to non-www redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_subdomain
+
+  def redirect_subdomain
+    redirect_to "https://fugu.lol#{request.fullpath}", status: :moved_permanently if
+      request.host == 'www.fugu.lol'
+  end
 end
