74dda658a076569c9108a1db1b7dac2abb2f4d1c
Can Olcer <shafyy@gmail.com>
2021-07-29 18:01:29 +0200

    handle empty events better

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,20 +4,27 @@ class ProjectsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def show
-    project = Project.find_by!(name: parse_project_name(params[:project_slug]))
+    project = Project.find_by(name: project_name_from_param)
+
+    # TODO
+    # redirect_to user_dashbaord unless project
+
     api_key = if params[:test] == 'true'
                 project.api_key_test
               else
                 project.api_key_live
               end
-    @events = Event
-              .where(
-                api_key: api_key,
-                name: params[:event].tr('-', ' ').titleize
-              )
-              .group("date_trunc('day', created_at)::date").count.sort.to_h
 
-    @event_names = Event.order(name: :asc).distinct.pluck(:name)
+    @event_names = Event.where(api_key: api_key).order(name: :asc).distinct.pluck(:name)
+
+    @events = unless @event_names.empty?
+                Event
+                  .where(
+                    api_key: api_key,
+                    name: event_name_from_param
+                  )
+                  .group("date_trunc('day', created_at)::date").count.sort.to_h
+              end
   end
 
   def create
@@ -26,7 +33,15 @@ class ProjectsController < ApplicationController
 
   private
 
-  def parse_project_name(project_name)
-    project_name.tr('-', ' ').titleize
+  def project_name_from_param
+    params[:project_slug].tr('-', ' ').titleize
+  end
+
+  def event_name_from_param
+    if params[:event]
+      params[:event].tr('-', ' ').titleize
+    else
+      @event_names.first
+    end
   end
 end
