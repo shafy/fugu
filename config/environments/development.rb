29c7ab80c826099e0b00be143bd871e2f08f3557
Can Olcer <canolcer@hey.com>
2021-08-12 16:45:19 +0000

    updated gitpod config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Allow requests from the Gitpod subdomain
+  config.hosts.clear
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
