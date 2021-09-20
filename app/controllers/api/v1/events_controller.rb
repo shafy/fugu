89ba934c68097229c4e624f230ecd7da8e2fb2e8
Can Olcer <canolcer@hey.com>
2021-09-20 12:51:19 +0000

    Added better param validation

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::EventsController < Api::ApiController
+  before_action :validate_param_keys, only: %w[create]
+
   def create
     event = Event.create(
       name: params[:name].titleize,
@@ -16,5 +18,9 @@ class Api::V1::EventsController < Api::ApiController
         error: event.errors.full_messages.first
       }, status: :unprocessable_entity
     end
+  end
+
+  def validate_param_keys
+    raise ArgumentError, "missing 'name' key" unless params.key?(:name)
   end
 end
