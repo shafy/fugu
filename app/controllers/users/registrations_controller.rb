c5b33f6664ef12b98131a4f18ee7d86fce09453e
Can Olcer <canolcer@hey.com>
2021-12-06 13:51:40 +0000

    added Fugu tracking to sign up and sign in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+    FuguService.track("Viewed Sign Up")
+  end
 
   def create
     super do |resource|
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,10 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+    FuguService.track("Viewed Sign In")
+  end
 
   # POST /resource/sign_in
   # def create
