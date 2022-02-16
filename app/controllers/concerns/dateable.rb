200dba65c60eb106f906a1b3526e94de89a7f884
Dorena <32681439+Buesi@users.noreply.github.com>
2022-02-16 10:56:06 +0100

    Buesi/add today and last months 38 (#39)

--- app/controllers/concerns/dateable.rb
+++ app/controllers/concerns/dateable.rb
@@ -6,15 +6,20 @@ module Dateable
   private
 
   def set_dates
-    @start_date = case params[:date]
+    # defaults to 7days if no date selected or if url param not in the list
+    @start_date = case params[:date] ? CGI.escapeHTML(params[:date]) : "7d"
                   when "30d"
                     29.days.ago
                   when "this_m"
                     Time.zone.now.beginning_of_month
                   when "6m"
                     6.months.ago
+                  when "3m"
+                    3.months.ago
                   when "12m"
                     12.months.ago
+                  when "1d"
+                    Time.zone.now
                   else
                     6.days.ago
                   end
