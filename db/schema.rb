183fe74a1a06c677ebe233dfe6fc1a4dd000bc83
Dorena <dorena@hey.com>
2022-02-28 13:46:17 +0000

    save latest selections in cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_191234) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_16_191234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "api_keys", force: :cascade do |t|
     t.string "key_value", null: false
     t.bigint "project_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "test", default: false
     t.index ["key_value"], name: "index_api_keys_on_key_value", unique: true
     t.index ["project_id"], name: "index_api_keys_on_project_id"
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2022_02_16_191234) do
   create_table "events", force: :cascade do |t|
     t.string "name", null: false
     t.jsonb "properties"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "api_key_id", null: false
     t.index ["api_key_id"], name: "index_events_on_api_key_id"
   end
@@ -38,24 +37,24 @@ ActiveRecord::Schema.define(version: 2022_02_16_191234) do
     t.string "event_name", null: false
     t.bigint "funnel_id", null: false
     t.integer "order"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["funnel_id"], name: "index_funnel_steps_on_funnel_id"
   end
 
   create_table "funnels", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "api_key_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["api_key_id"], name: "index_funnels_on_api_key_id"
     t.index ["name", "api_key_id"], name: "index_funnels_on_name_and_api_key_id", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["name", "user_id"], name: "index_projects_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
@@ -65,14 +64,14 @@ ActiveRecord::Schema.define(version: 2022_02_16_191234) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "confirmation_token"
-    t.datetime "confirmed_at", precision: 6
-    t.datetime "confirmation_sent_at", precision: 6
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "stripe_customer_id"
     t.integer "status", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
