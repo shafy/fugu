ad750dce2d8579946a8070e6f2399c2e1f4ddd0a
Can Olcer <canolcer@hey.com>
2022-05-21 13:45:47 +0000

    hotfix: better error handling when calling Fugu API

--- app/services/fugu_service.rb
+++ app/services/fugu_service.rb
@@ -12,9 +12,14 @@ class FuguService
       properties: properties
     }
 
-    Faraday.post(ENV.fetch("FUGU_URL", nil)) do |req|
-      req.headers["Content-Type"] = "application/json"
-      req.body = body.to_json
+    begin
+      Faraday.post(ENV.fetch("FUGU_URLd", nil)) do |req|
+        req.headers["Content-Type"] = "application/json"
+        req.body = body.to_json
+      end
+    rescue StandardError => e
+      logger = Logger.new(STDOUT)
+      logger.error("Error while calling Fugu API: #{e}")
     end
   end
 end
